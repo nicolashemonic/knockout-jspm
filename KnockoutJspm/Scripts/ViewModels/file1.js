8d7b207c407505eb891cf8cd0e0dc01e7fccba7d
Nicolas Hémonic <nicolas.hemonic@gmail.com>
2016-01-07 11:24:26 +0100

    Remove generated scripts

--- KnockoutJspm/Scripts/ViewModels/file1.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", 'underscore'], function (require, exports, foo) {
-    var bar = {
-        example: foo.first([])
-    };
-    return bar;
-});
-//# sourceMappingURL=file1.js.map
